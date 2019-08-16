da6986a5c078fdc9f37ecd5bb4ff562861e1baf5
Bartender <31946573+diego98512@users.noreply.github.com>
2019-08-16 14:12:37 -0400

    Create select_music.sh
    To be use in tandem with Tizonia. First upload shows echo statements as the playing statements. They just need to be changed to playlists and Tizonia.

---
+++ Shell Files/select_music.sh
@@ -0,0 +1,487 @@
+#!/bin/bash
+
+clear
+
+echo "
+0. Quit
+
+1.  Zelda
+2.  Metroid
+3.  Mario
+4.  Donkey Kong
+5.  Final Fantasy
+6.  Tales Of
+7.  Lunar
+8.  Chrono Trigger
+9.  Animal Crossing
+10. Super Smash Bros.
+
+
+
+"
+
+read -p "Which series would you like to listen to music from? " series_selection
+
+while :
+do
+    case $series_selection in
+
+        0)
+            break
+            ;;
+        1)
+            # Insert Zelda Selections
+            clear
+
+            echo "
+1.  The Legend of Zelda
+2.  The Legend of Zelda: The Adventure of Link
+3.  The Legend of Zelda: A Link to the Past
+4.  The Legend of Zelda: Ocarina of Time
+5.  The Legend of Zelda: Majora's Mask
+6.  The Legend of Zelda: Oracle of Seasons
+7.  The Legend of Zelda: Oracle of Ages
+8.  The Legend of Zelda: Minish Cap
+9.  The Legend of Zelda: The Wind Waker
+10. The Legend of Zelda: Twilight Princess
+11. The Legend of Zelda: The Phantom Hourglass
+12. The Legend of Zelda: Spirit Tracks
+13. The Legend of Zelda: Skyward Sword
+14. The Legend of Zelda: Link's Awakening
+15. The Legend of Zelda: Breath of the Wild
+            "
+            read -p "Which game would you like to listen to music from? " game_selection
+
+            while :
+            do
+                case $game_selection in
+
+                    1)
+                        clear
+                        echo "The Legend of Zelda"
+                        break
+                        ;;
+                    2)
+                        clear
+                        echo "The Legend of Zelda: The Adventure of Link"
+                        break
+                        ;;
+                    3)
+                        clear
+                        echo "The Legend of Zelda: A Link to the Past"
+                        break
+                        ;;
+                    4)
+                        clear
+                        echo "The Legend of Zelda: Ocarina of Time"
+                        break
+                        ;;
+                    5)
+                        clear
+                        echo "The Legend of Zelda: Majora's Mask"
+                        break
+                        ;;
+                    6)
+                        clear
+                        echo "The Legend of Zelda: Oracle of Seasons"
+                        break
+                        ;;
+                    7)
+                        clear
+                        echo "The Legend of Zelda: Oracle of Ages"
+                        break
+                        ;;
+                    8)
+                        clear
+                        echo "The Legend of Zelda: Minish Cap"
+                        break
+                        ;;
+                    9)
+                        clear
+                        echo "The Legend of Zelda: The Wind Waker"
+                        break
+                        ;;
+                    10)
+                        clear
+                        echo "The Legend of Zelda: Twilight Princess"
+                        break
+                        ;;
+                    11)
+                        clear
+                        echo "The Legend of Zelda: The Phantom Hourglass"
+                        break
+                        ;;
+                    12)
+                        clear
+                        echo "The Legend of Zelda: Spirit Tracks"
+                        break
+                        ;;
+                    13)
+                        clear
+                        echo "The Legend of Zelda: Skyward Sword"
+                        break
+                        ;;
+                    14)
+                        clear
+                        echo "The Legend of Zelda: Link's Awakening"
+                        break
+                        ;;
+                    15)
+                        clear
+                        echo "The Legend of Zelda: Breath of the Wild"
+                        break
+                        ;;
+                    *)
+                        echo "That's not a valid selection..."
+                        ;;
+                    esac
+                done
+            break
+            ;;
+        2)
+            # Insert Metroid Selections
+echo "
+1. Super Metroid
+2. Metroid Fusion
+3. Metroid: Zero Mission
+4. Metroid Prime
+5. Metroid: Return of Samus
+
+
+
+"
+            read -p "Which game would you like to listen to music from? " game_selection
+
+            while :
+            do
+                case $game_selection in
+                    1)
+                        clear
+                        echo "Super Metroid"
+                        break
+                        ;;
+                    2)
+                        clear
+                        echo "Metroid Fusion"
+                        break
+                        ;;
+                    3)
+                        clear
+                        echo "Metroid: Zero Mission"
+                        break
+                        ;;
+                    4)
+                        clear
+                        echo "Metroid Prime"
+                        break
+                        ;;
+                    5)
+                        clear
+                        echo "Metroid: Return of Samus"
+                        break
+                        ;;
+                    *)
+                        echo "That's not a valid selection..."
+                        ;;
+                esac
+            done
+            break
+            ;;
+        3)
+            # Insert Mario Selections
+
+echo "
+1. Super Mario Sunshine
+2. Super Mario Odyssey
+3. Super Mario 64
+
+
+
+"
+            read -p "Which game would you like to listen to music from? " game_selection
+
+            while :
+            do
+                case $game_selection in
+                    1)
+                        clear
+                        echo "Super Mario Sunshine"
+                        break
+                        ;;
+                    2)
+                        clear
+                        echo "Super Mario Odyssey"
+                        break
+                        ;;
+                    3)
+                        clear
+                        echo "Super Mario 64"
+                        break
+                        ;;
+                    *)
+                        echo "That's not a valid selection..."
+                        ;;
+                esac
+            done
+            break
+            ;;
+        4)
+            # Insert Donkey Kong Selections
+
+echo "
+1. Donkey Kong 64
+
+
+
+"
+
+            while :
+            do
+                case $game_selection in
+                    1)
+                        clear
+                        echo "Donkey Kong 64"
+                        break
+                        ;;
+                    *)
+                        echo "That's not a valid selection..."
+                        ;;
+                esac
+            done
+            break
+            ;;
+        5)
+            # Insert Final Fantasy Selections
+
+echo "
+1.  Final Fantasy I
+2.  Final Fantasy II
+3.  Final Fantasy III
+4.  Final Fantasy IV
+5.  Final Fantasy V
+6.  Final Fantasy VI
+7.  Final Fantasy VII
+8.  Final Fantasy VIII
+9.  Final Fantasy IX
+10. Final Fantasy X
+11. Final Fantasy XI
+12. Final Fantasy XII
+13. Final Fantasy XIII
+14. Final Fantasy XIV
+15. Final Fantasy XV
+16. Final Fantasy: Crystal Chronicles
+
+
+
+"
+            read -p "Which game would you like to listen to music from? " game_selection
+
+            while :
+            do
+                case $game_selection in
+                    1)
+                        clear
+                        echo "Final Fantasy I"
+                        break
+                        ;;
+                    2)
+                        clear
+                        echo "Final Fantasy II"
+                        break
+                        ;;
+                    3)
+                        clear
+                        echo "Final Fantasy III"
+                        break
+                        ;;
+                    4)
+                        clear
+                        echo "Final Fantasy IV"
+                        break
+                        ;;
+                    5)
+                        clear
+                        echo "Final Fantasy V"
+                        break
+                        ;;
+                    6)
+                        clear
+                        echo "Final Fantasy VI"
+                        break
+                        ;;
+                    7)
+                        clear
+                        echo "Final Fantasy VII"
+                        break
+                        ;;
+                    8)
+                        clear
+                        echo "Final Fantasy VIII"
+                        break
+                        ;;
+                    9)
+                        clear
+                        echo "Final Fantasy IX"
+                        break
+                        ;;
+                    10)
+                        clear
+                        echo "Final Fantasy X"
+                        break
+                        ;;
+                    11)
+                        clear
+                        echo "Final Fantasy XI"
+                        break
+                        ;;
+                    12)
+                        clear
+                        echo "Final Fantasy XII"
+                        break
+                        ;;
+                    13)
+                        clear
+                        echo "Final Fantasy XIII"
+                        break
+                        ;;
+                    14)
+                        clear
+                        echo "Final Fantasy XIV"
+                        break
+                        ;;
+                    15)
+                        clear
+                        echo "Final Fantasy XV"
+                        break
+                        ;;
+                    16)
+                        clear
+                        echo "Final Fantasy: Crystal Chronicles"
+                        break
+                        ;;
+                    *)
+                        echo "That's not a valid selection..."
+                        ;;
+                esac
+            done
+            break
+            ;;
+        6)
+            # Insert Tales Of Selection
+
+echo "
+1. Tales of Zestiria
+
+
+
+"
+
+            read -p "Which game would you like to listen to music from? " game_selection
+
+            while :
+            do
+                case $game_selection in
+                    1)
+                        clear
+                        echo "Tales of Zestiria"
+                        break
+                        ;;
+                    *)
+                        echo "That's not a valid selection..."
+                        ;;
+                esac
+            done
+            break
+            ;;
+        7)
+            # Insert Lunar Selection
+
+echo "
+1. Lunar Silver Star Harmony
+2. Lunar Dragon Song
+
+
+
+"
+
+            read -p "Which game would you like to listen to music from? " game_selection
+
+            while :
+            do
+                case $game_selection in
+                    1)
+                        clear
+                        echo "Lunar Silver Star Harmony"
+                        break
+                        ;;
+                    2)
+                        clear
+                        echo "Lunar Dragon Song"
+                        break
+                        ;;
+                    *)
+                        echo "That's not a valid selection..."
+                        ;;
+                esac
+            done
+            break
+            ;;
+        8)
+            # Insert Chrono Trigger Selection
+
+            echo "Chrono Trigger"
+            break
+            ;;
+        9)
+            # Insert Animal Crossing Selection
+
+            echo "Animal Crossing"
+            break
+            ;;
+        10)
+            # Insert Super Smash Bros Selection
+
+echo "
+1. Super Smash Bros.
+2. Super Smash Bros. Melee
+3. Super Smash Bros. Brawl
+4. Super Smash Bros. Ultimate
+
+
+
+"
+
+            read -p "Which game do you want to listen to music from? " game_selection
+
+            while :
+            do
+                case $game_selection in
+                    1)
+                        clear
+                        echo "Super Smash Bros."
+                        break
+                        ;;
+                    2)
+                        clear
+                        echo "Super Smash Bros. Melee"
+                        break
+                        ;;
+                    3)
+                        clear
+                        echo "Super Smash Bros. Brawl"
+                        break
+                        ;;
+                    4)
+                        clear
+                        echo "Super Smash Bros. Ultimate"
+                        break
+                        ;;
+                    *)
+                        echo "That's not a valid selection..."
+                        ;;
+                esac
+            done
+            break
+            ;;
+    esac
+done
