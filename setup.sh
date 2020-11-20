293740c1cc34df0367feba905e4fc0c3706cad83
diego <diegopflorez@outlook.com>
2020-11-20 00:58:52 -0500

    Added a setup script that copies the dot files and gives the option to join a domain. Will add more features as needed.

---
+++ setup.sh
@@ -0,0 +1,17 @@
+u="$USER"
+
+echo "Copying dot files..."
+cp Dots/.* /home/$user/ -y
+echo "Dot files copied."
+
+echo ""
+
+read -p 'Would you like to join this computer to a domain? [Y/n]: ' dom_join
+if [ $dom_join -eq 'y' ]
+	sudo apt -y install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
+	read -p "What's the name of the domain you'd like to join? \b" dom_name
+	sudo realm join $dom_name
+	sudo echo "session optional        pam_mkhomedir.so skel=/etc/skel umask=077" > /etc/pam.d/common-session
+	sudo sed 's#use_fully_qualified_names = True#use_fully_qualified_names = False' /etc/sssd/sssd.conf
+	sudo systemctl restart sssd
+fi
