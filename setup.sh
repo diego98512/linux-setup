1cd281db002f3357da4fe30a44780f3ea993a9d2
Diego <diego@Wallmount.skunkworks.loc>
2020-11-28 02:42:42 -0500

    Fixed a spacing issue in the setup script near the domain join domain name.

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ read -p 'Would you like to join this computer to a domain? [Y/n]: ' dom_join
 if [ $dom_join = 'y' ]
 then
 	sudo apt -y install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
-	read -p "What's the name of the domain you'd like to join?" dom_name
+	read -p "What's the name of the domain you'd like to join: " dom_name
 	sudo realm join $dom_name
 	sudo echo "session optional        pam_mkhomedir.so skel=/etc/skel umask=077" > /etc/pam.d/common-session
 	sudo sed 's#use_fully_qualified_names = True#use_fully_qualified_names = False' /etc/sssd/sssd.conf
